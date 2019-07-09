7503931d0c66580a02d02b0eded0e2506483fa3e
Bader <mrbadernasser@gmail.com>
2019-07-09 18:24:18 +0300

    First Working Website

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def home_page(request):
+	return HttpResponse("<h1> Hello</h1>")
+
+def any_thing(request):
+	return render(request,"any_thing.html")
--- task_02/urls.py
+++ task_02/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from restaurants import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',,name='hello-world'),
+    path('ayshy',views.any_thing,name='Whatever'),
+    path('home/', views.home_page,name='home')
 ]
